c6964b6246317f4741b2e6833f570d3daa7001f7
blaketillema <61172664+blaketillema@users.noreply.github.com>
2024-03-19 10:28:24 +1000

    python3

--- img2html.py
+++ img2html.py
@@ -30,16 +30,16 @@ pixels = image.load()
 width, height = image.size
 
 # Start table
-print '<html><body>'
-print '<table width="%spx" height="%spx" cellpadding="0" cellspacing="0">' % (width, height)
+print('<html><body>')
+print('<table width="%spx" height="%spx" cellpadding="0" cellspacing="0">' % (width, height))
 
 # Loop through rows
 run_length = 1
-for row in xrange(height):
-    print '<tr>'
+for row in range(height):
+    print('<tr>')
     
     # Loop through columns
-    for col in xrange(width):
+    for col in range(width):
 
         # If this is not first row (to make sure we get all columns), the last column, and this is part of a run, increase colspan
         if row != 0 and col < width - 1 and pixels[col, row] == pixels[col + 1, row]:
@@ -49,11 +49,11 @@ for row in xrange(height):
         else:
             color = '#%x%x%x' % pixels[col, row]
             colspan = 'colspan="%s"' % run_length if run_length > 1 else ''
-            print '<td bgcolor="%s" width="%spx" height="1px" %s></td>' % (color, run_length, colspan),
+            print('<td bgcolor="%s" width="%spx" height="1px" %s></td>' % (color, run_length, colspan),)
             run_length = 1
 
-    print '</tr>'
+    print('</tr>')
 
 # Finish up
-print '</table>'
-print '</body></html>'
+print('</table>')
+print('</body></html>')
